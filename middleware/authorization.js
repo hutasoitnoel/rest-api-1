0a2ef549db2a2c49ffaeefff61fcdf5dfbfa78e2
qoyyima22 <qoyyimafiassalam@gmail.com>
2019-04-01 20:49:56 +0700

    Qoyyima Salam

---
+++ middleware/authorization.js
@@ -0,0 +1,8 @@
+module.exports = function(req,res,next){
+    if(req.authenticatedUser.role === 'admin'){
+        next()
+    }
+    else {
+        res.status(403).send(`Anda bukan Admin!`)
+    }
+}
---
+++ middleware/authorizationAdminAuthen.js
@@ -0,0 +1,8 @@
+module.exports = function(req,res,next){
+    if(req.authenticatedUser || req.authenticatedUser.role === 'admin'){
+        next()
+    }
+    else {
+        res.status(403).send(`Anda tidak memiliki akses!`)
+    }
+}
